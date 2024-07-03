888d89dfef86014faaae5d9ab88d89bf865d4f26
Mohammed Sinan Mp <sinan.m.p333@gmail.com>
2024-07-03 16:46:18 +0530

    Readme file added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -7,15 +7,15 @@ function Banner() {
   const [showModal, setShowModal] = useState(false);
 
   return (
-    <div style={{backgroundImage:`url("/assets/images/banner.jpg")`}} className='banner'>
+    <div style={{backgroundImage:`url("/assets/images/banner.jpeg")`}} className='banner'>
       <MovieDataModal showModal={showModal} setShowModal={setShowModal}/>
       <div className='content'>
-        <h1 className='title'>hello world</h1>
-        <h1 className='discription'>haai</h1>
+        <h1 className='title'>Movie Watch list Application</h1>
+        <h1 className='discription'>The Movie Watchlist application is a web-based tool that allows users to manage a list of movies they want to watch. Users can add, edit, and delete movies from their watchlist, mark movies as watched or unwatched, and rate and review movies. State management will be handled using Redux to ensure efficient and predictable state updates.</h1>
         <br />
         <div className='banner-buttons'>
-            <button className='banner-buttoon1' onClick={()=>setShowModal(!showModal)}>Add</button>
-            <button className='banner-buttoon2'>More Info</button>
+            <button className='banner-buttoon1' onClick={()=>setShowModal(!showModal)}>Add Movie</button>
+            <a href="https://github.com/Sinan0333/Movie-Watchlist-Application" target="_blank" rel="noreferrer"><button className='banner-buttoon2'>More Info</button></a>
         </div>
       </div> 
       <div className="fade_bottom"></div>
